d69c5b4f09b0a4a36c803e546d2913b4e2ca1a50
Jérémy Kervran <jkerran@adimeo.com>
2025-07-08 15:20:08 +0200

    Add header & footer

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer: React.FC = () => {
-  return <div className={styles.Footer} data-testid="Footer"></div>;
+  return <div className={styles.Footer} data-testid="Footer">
+    Copyright Kervran 2025
+  </div>;
 };
 
 export default Footer;
--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import styles from "./Header.module.css";
 
 const Header: React.FC = () => {
-  return <div className={styles.Header} data-testid="Header"></div>;
+  return <div className={styles.Header} data-testid="Header">
+    <span className={styles.meme}>Meme</span>.
+    <span className={styles.js}>js</span>
+  </div>;
 };
 
 export default Header;
